647a0bbf3c6c41925da0dab9536fce805a151909
KIRINn5 <141363578+KIRINn5@users.noreply.github.com>
2024-06-28 00:05:30 +0300

    commit-0.1

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify([], null));
+    console.log('Successfully removed all contacts');
+  } catch (error) {
+    console.log(error, 'Failed to remove contacts');
+  }
+};
 
-removeAllContacts();
+await removeAllContacts();
